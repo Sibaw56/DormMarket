48fedddd80df165719ecaf7862d16a2099c0b2e7
Sibaw56 <161341965+Sibaw56@users.noreply.github.com>
2026-07-06 15:17:52 +0800

    Update BrowseDetailStudent.cs

--- BrowseDetailStudent.cs
+++ BrowseDetailStudent.cs
@@ -24,10 +24,58 @@ namespace DormMarket
             message.Show();
             this.Hide();
         }
-        private void btnProfile_Click((object sender, EventArgs e)
+        private void ManangeProfileStudent_Load(object sender, EventArgs e)//Load profile infos
         {
-            var profile = new ManageProfileStudent();
-            profile.Show();
+            //Load the profile infos
+        }
+
+        private void btnChangePhoto_Click(object sender, EventArgs e) //change photo button
+        {
+            
+        }
+
+        private void btnSaveChanges_Click(object sender, EventArgs e) //save changes button
+        {
+            if(string.IsNullOrEmpty(txtName.Text))
+            {
+                txtName.Text = //database value Name;
+                return;
+            }
+            if (string.IsNullOrEmpty(txtPhone.Text))
+            {
+                txtPhone.Text = //database value Phone;
+                return;
+            }
+            if (string.IsNullOrEmpty(txtEmail.Text))
+            {
+                txtEmail.Text = //database value Phone;
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtAddress.Text))
+            {
+                txtAddress.Text = //database value Phone;
+                return;
+            }
+            if (string.IsNullOrEmpty(txtPassword.Text))
+            {
+                txtPassword.Text = //database value Phone;
+                return;
+            }
+            //edit a row of student profile database with the new values from the textboxes
+        }
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+            var role = new Role();
+            role.Show();
+            this.Hide();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            var browse = new BrowseDormStudent();
+            browse.Show();
             this.Hide();
         }
     }
